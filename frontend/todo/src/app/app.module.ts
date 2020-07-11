1169fe2b80864cc527bb212a067b05ddb8540607
[navmich] <[navmich01@gmail.com]>
2020-07-11 19:54:04 +0200

    Pridani Bootstrap css + header + footer components

--- frontend/todo/src/app/app.module.ts
+++ frontend/todo/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { SecondWelcomeComponent } from "./second-welcome/second-welcome.componen
 import { LoginComponent } from "./login/login.component";
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { ListTodosComponent } from './list-todos/list-todos.component';
     LoginComponent,
     ErrorComponent,
     ListTodosComponent,
+    MenuComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
